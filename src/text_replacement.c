eec350597500ce2e0674793a5225099cb5e156b5
tomtee <33041004+t0mtee@users.noreply.github.com>
2025-05-02 15:39:33 +0100

    Add references to D-pad usage through text replacement. Requires EZTR.

---
+++ src/text_replacement.c
@@ -0,0 +1,159 @@
+#include "eztr_api.h"
+
+EZTR_ON_INIT void replace_msgs() {
+    EZTR_Basic_ReplaceText(
+        0x003E,
+        EZTR_TRANSLUSCENT_BLUE_TEXT_BOX,
+        0,
+        EZTR_ICON_DUNGEON_MAP,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "|17You found the |01Dungeon Map|00!|18|11|1F|00|0APress START to open the|11subscreens and view the Map|11Screen.|10Areas on the map that appear in|11|03blue|00 are places you have visited.|11Your current location is the |11|05flashing|00 area.|10Use |BB or the |04D-pad|00 to view|11another floor.|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x0450,
+        EZTR_STANDARD_TEXT_BOX_II,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "How much? How much? |11|CC|11Set the amount with |BB or the |04D-pad|00|11and press |B0 to decide.|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x046E,
+        EZTR_STANDARD_TEXT_BOX_II,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "How much do you want?|11|CC|11Set the amount with |BB or the |04D-pad|00|11and press |B0 to decide.|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x0640,
+        EZTR_STANDARD_TEXT_BOX_II,
+        49,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "View items with |BB or the |04D-pad|00.|11|11|02|C2Talk to the shopkeeper|11Leave|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x0729,
+        EZTR_STANDARD_TEXT_BOX_II,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "What's the code?|11|01|D1|11|00Pick numbers with |BB or the |04D-pad|00|11and press |B0 to decide.|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x083E,
+        EZTR_STANDARD_TEXT_BOX_II,
+        49,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "Use |BB or the |04D-pad|00 to browse.|11|11|02|C2Speak to the hag|11Quit shopping|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x0BC0,
+        EZTR_STANDARD_TEXT_BOX_II,
+        49,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "Use |BB or the |04D-pad|00 to view my|11goods.|11|02|C2Talk to shopkeeper|11Quit shopping|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x12D6,
+        EZTR_STANDARD_TEXT_BOX_II,
+        49,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "View products with |BB or the |04D-pad|00.|11|11|02|C2Speak to shopkeeper|11Quit shopping|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x29D6,
+        EZTR_STANDARD_TEXT_BOX_II,
+        49,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "Use |BB or the |04D-pad|00 to browse.|11|11|02|C2Talk to shopkeeper|11Quit shopping|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x2A00,
+        EZTR_INVISIBLE_TEXT_BOX_IV,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "|BB or |04D-pad|00 Move   |B1 Quit|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x2B5F,
+        EZTR_STANDARD_TEXT_BOX_II,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "OK. Enter your number.|11|02 |D5|11|00Select numbers with |BB or the |04D-pad|00.|11Press |B0 to confirm.|BF",
+        NULL
+    );
+
+    EZTR_Basic_ReplaceText(
+        0x3528,
+        EZTR_STANDARD_TEXT_BOX_II,
+        1,
+        EZTR_ICON_NO_ICON,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        EZTR_NO_VALUE,
+        true,
+        "|1Ei|11How much will you |01bet|00?|11|D0|11Pick numbers with |BB or the |04D-pad|00.|11Press |B0 to decide.|BF",
+        NULL
+    );
+}
